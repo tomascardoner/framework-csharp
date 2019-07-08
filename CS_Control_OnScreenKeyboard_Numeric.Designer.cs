6e494cc46594aaa1b926e12f7d7a619f07497a3c
Tomas A. Cardoner <tomas@cardoner.com.ar>
2019-07-08 16:59:28 -0300

    On screen numeric keyboard added

---
+++ CS_Control_OnScreenKeyboard_Numeric.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace CSTransporteKiosk
+{
+    partial class CS_Control_OnScreenKeyboard_Numeric
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelKey9 = new System.Windows.Forms.Label();
+            this.labelKey8 = new System.Windows.Forms.Label();
+            this.labelKey7 = new System.Windows.Forms.Label();
+            this.labelKey6 = new System.Windows.Forms.Label();
+            this.labelKey5 = new System.Windows.Forms.Label();
+            this.labelKey4 = new System.Windows.Forms.Label();
+            this.labelKey3 = new System.Windows.Forms.Label();
+            this.labelKey2 = new System.Windows.Forms.Label();
+            this.labelKey1 = new System.Windows.Forms.Label();
+            this.labelKey0 = new System.Windows.Forms.Label();
+            this.labelKeyDelete = new System.Windows.Forms.Label();
+            this.labelKeyClear = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Controls.Add(this.labelKeyClear, 2, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelKeyDelete, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey0, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey1, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey2, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey4, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey5, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey6, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey7, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey8, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.labelKey9, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(210, 280);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // labelKey9
+            // 
+            this.labelKey9.AutoSize = true;
+            this.labelKey9.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey9.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey9.Location = new System.Drawing.Point(6, 3);
+            this.labelKey9.Name = "labelKey9";
+            this.labelKey9.Size = new System.Drawing.Size(59, 66);
+            this.labelKey9.TabIndex = 0;
+            this.labelKey9.Text = "9";
+            this.labelKey9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey8
+            // 
+            this.labelKey8.AutoSize = true;
+            this.labelKey8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey8.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey8.Location = new System.Drawing.Point(74, 3);
+            this.labelKey8.Name = "labelKey8";
+            this.labelKey8.Size = new System.Drawing.Size(59, 66);
+            this.labelKey8.TabIndex = 1;
+            this.labelKey8.Text = "8";
+            this.labelKey8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey7
+            // 
+            this.labelKey7.AutoSize = true;
+            this.labelKey7.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey7.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey7.Location = new System.Drawing.Point(142, 3);
+            this.labelKey7.Name = "labelKey7";
+            this.labelKey7.Size = new System.Drawing.Size(62, 66);
+            this.labelKey7.TabIndex = 2;
+            this.labelKey7.Text = "7";
+            this.labelKey7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey6
+            // 
+            this.labelKey6.AutoSize = true;
+            this.labelKey6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey6.Location = new System.Drawing.Point(6, 72);
+            this.labelKey6.Name = "labelKey6";
+            this.labelKey6.Size = new System.Drawing.Size(59, 66);
+            this.labelKey6.TabIndex = 3;
+            this.labelKey6.Text = "6";
+            this.labelKey6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey5
+            // 
+            this.labelKey5.AutoSize = true;
+            this.labelKey5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey5.Location = new System.Drawing.Point(74, 72);
+            this.labelKey5.Name = "labelKey5";
+            this.labelKey5.Size = new System.Drawing.Size(59, 66);
+            this.labelKey5.TabIndex = 4;
+            this.labelKey5.Text = "5";
+            this.labelKey5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey4
+            // 
+            this.labelKey4.AutoSize = true;
+            this.labelKey4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey4.Location = new System.Drawing.Point(142, 72);
+            this.labelKey4.Name = "labelKey4";
+            this.labelKey4.Size = new System.Drawing.Size(62, 66);
+            this.labelKey4.TabIndex = 5;
+            this.labelKey4.Text = "4";
+            this.labelKey4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey3
+            // 
+            this.labelKey3.AutoSize = true;
+            this.labelKey3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey3.Location = new System.Drawing.Point(6, 141);
+            this.labelKey3.Name = "labelKey3";
+            this.labelKey3.Size = new System.Drawing.Size(59, 66);
+            this.labelKey3.TabIndex = 6;
+            this.labelKey3.Text = "3";
+            this.labelKey3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey2
+            // 
+            this.labelKey2.AutoSize = true;
+            this.labelKey2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey2.Location = new System.Drawing.Point(74, 141);
+            this.labelKey2.Name = "labelKey2";
+            this.labelKey2.Size = new System.Drawing.Size(59, 66);
+            this.labelKey2.TabIndex = 7;
+            this.labelKey2.Text = "2";
+            this.labelKey2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey1
+            // 
+            this.labelKey1.AutoSize = true;
+            this.labelKey1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey1.Location = new System.Drawing.Point(142, 141);
+            this.labelKey1.Name = "labelKey1";
+            this.labelKey1.Size = new System.Drawing.Size(62, 66);
+            this.labelKey1.TabIndex = 8;
+            this.labelKey1.Text = "1";
+            this.labelKey1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKey0
+            // 
+            this.labelKey0.AutoSize = true;
+            this.labelKey0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKey0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKey0.Location = new System.Drawing.Point(6, 210);
+            this.labelKey0.Name = "labelKey0";
+            this.labelKey0.Size = new System.Drawing.Size(59, 67);
+            this.labelKey0.TabIndex = 9;
+            this.labelKey0.Text = "0";
+            this.labelKey0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKeyDelete
+            // 
+            this.labelKeyDelete.AutoSize = true;
+            this.labelKeyDelete.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKeyDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKeyDelete.Location = new System.Drawing.Point(74, 210);
+            this.labelKeyDelete.Name = "labelKeyDelete";
+            this.labelKeyDelete.Size = new System.Drawing.Size(59, 67);
+            this.labelKeyDelete.TabIndex = 10;
+            this.labelKeyDelete.Text = "borrar";
+            this.labelKeyDelete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelKeyClear
+            // 
+            this.labelKeyClear.AutoSize = true;
+            this.labelKeyClear.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelKeyClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelKeyClear.Location = new System.Drawing.Point(142, 210);
+            this.labelKeyClear.Name = "labelKeyClear";
+            this.labelKeyClear.Size = new System.Drawing.Size(62, 67);
+            this.labelKeyClear.TabIndex = 11;
+            this.labelKeyClear.Text = "limpiar";
+            this.labelKeyClear.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CS_Control_OnScreenKeyboard_Numeric
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "CS_Control_OnScreenKeyboard_Numeric";
+            this.Size = new System.Drawing.Size(210, 280);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label labelKey7;
+        private System.Windows.Forms.Label labelKey8;
+        private System.Windows.Forms.Label labelKey9;
+        private System.Windows.Forms.Label labelKeyClear;
+        private System.Windows.Forms.Label labelKeyDelete;
+        private System.Windows.Forms.Label labelKey0;
+        private System.Windows.Forms.Label labelKey1;
+        private System.Windows.Forms.Label labelKey2;
+        private System.Windows.Forms.Label labelKey3;
+        private System.Windows.Forms.Label labelKey4;
+        private System.Windows.Forms.Label labelKey5;
+        private System.Windows.Forms.Label labelKey6;
+    }
+}
