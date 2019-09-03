2d80ed3ca53ddac93017c889f53971d1a8d8c84c
Tomás A. Cardoner <tomas@cardoner.com.ar>
2019-09-03 12:04:09 -0300

    OnScreenKeyboardAlphanumeric added

---
+++ Controls.OnScreenKeyboardAlphanumeric.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace CardonerSistemas
+{
+    partial class ControlsOnScreenKeyboardAlphanumeric
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelKeyboard = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // panelKeyboard
+            // 
+            this.panelKeyboard.ColumnCount = 1;
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+            this.panelKeyboard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelKeyboard.Location = new System.Drawing.Point(0, 0);
+            this.panelKeyboard.Name = "panelKeyboard";
+            this.panelKeyboard.RowCount = 1;
+            this.panelKeyboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelKeyboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelKeyboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelKeyboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelKeyboard.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.panelKeyboard.Size = new System.Drawing.Size(667, 294);
+            this.panelKeyboard.TabIndex = 0;
+            // 
+            // ControlsOnScreenKeyboardAlphanumeric
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelKeyboard);
+            this.Name = "ControlsOnScreenKeyboardAlphanumeric";
+            this.Size = new System.Drawing.Size(667, 294);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panelKeyboard;
+    }
+}
---
+++ Controls.OnScreenKeyboardAlphanumeric.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows.Forms;
+
+namespace CardonerSistemas
+{
+    public partial class ControlsOnScreenKeyboardAlphanumeric : UserControl
+    {
+        private const int _KeyboardRows = 4;
+        private const int _KeyboardColumns = 11;
+        private const string _KeyButtonNamePrefix = "buttonKey";
+        private const string _KeyButtonNameRowPrefix = "R";
+        private const string _KeyButtonNameColumnPrefix = "C";
+
+        private string[,] _KeyboardKeys = new string[,]
+        {
+            { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "BS" },
+            { "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "DEL" },
+            { "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ñ", "Ç" },
+            { "Z", "X", "C", "V", "B", "N", "M", "SPACE", "SPACE", "'", "Ü" }
+        };
+
+        public ControlsOnScreenKeyboardAlphanumeric()
+        {
+            InitializeComponent();
+
+            CreateKeyboard();
+        }
+
+        private void CreateKeyboard()
+        {
+            CreatePanel();
+            CreateButtons();
+        }
+
+        private void CreatePanel()
+        {
+            panelKeyboard.RowCount = _KeyboardRows;
+            panelKeyboard.ColumnCount = _KeyboardColumns;
+        }
+
+        private void CreateButtons()
+        {
+            // Rows
+            for (int row = 0; row < _KeyboardRows; row++)
+            {
+                Button previousButton = null;
+                int previousColumnSpan = 1;
+
+                // Columns
+                for (int column = 0; column < _KeyboardColumns; column++)
+                {
+                    if (previousButton != null && previousButton.Text == _KeyboardKeys[row, column])
+                    {
+                        previousColumnSpan++;
+                        panelKeyboard.SetColumnSpan(previousButton, previousColumnSpan);
+                    }
+                    else
+                    {
+                        Button button = new Button();
+                        button.Name = string.Format("{0}{1}{2}{3}{4}", _KeyButtonNamePrefix, row, _KeyButtonNameRowPrefix , column, _KeyButtonNameColumnPrefix);
+                        button.Text = _KeyboardKeys[row, column];
+                        panelKeyboard.Controls.Add(button, column, row);
+                        button.Dock = DockStyle.Fill;
+                        previousButton = button;
+                        previousColumnSpan = 1;
+                    }
+                }
+            }
+        }
+    }
+}
