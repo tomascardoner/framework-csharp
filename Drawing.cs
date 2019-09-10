f94174877d7e03b1b1821235041903298cf11916
Tomás A. Cardoner <tomas@cardoner.com.ar>
2019-09-10 20:38:20 -0300

    Create Drawing.cs

---
+++ Drawing.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace CardonerSistemas
+{
+    class Drawing
+    {
+        static void DrawRoundedRectangle(Graphics g, Pen p, int x, int y, int w, int h, int rx, int ry)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddArc(x, y, rx + rx, ry + ry, 180, 90);
+            path.AddLine(x + rx, y, x + w - rx, y);
+            path.AddArc(x + w - 2 * rx, y, 2 * rx, 2 * ry, 270, 90);
+            path.AddLine(x + w, y + ry, x + w, y + h - ry);
+            path.AddArc(x + w - 2 * rx, y + h - 2 * ry, rx + rx, ry + ry, 0, 91);
+            path.AddLine(x + rx, y + h, x + w - rx, y + h);
+            path.AddArc(x, y + h - 2 * ry, 2 * rx, 2 * ry, 90, 91);
+            path.CloseFigure();
+            g.DrawPath(p, path);
+        }
+    }
+}
