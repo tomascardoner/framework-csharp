78d68deaae75d1af21686fc9ce7d6732d2e519f8
Tomás A. Cardoner <tomas@cardoner.com.ar>
2019-09-18 12:47:00 -0300

    Update Database.Framework.Lite.cs

--- Database.Framework.Lite.cs
+++ Database.Framework.Lite.cs
@@ -4,24 +4,60 @@ namespace CardonerSistemas.Database.Framework
 {
     static partial class Lite
     {
+
+        #region Gender
+
+        private const string EntityFemaleArticle = "la";
+        private const string EntityFemalePluralArticle = "las";
+        private const string EntityMaleArticle = "el";
+        private const string EntityMalePluralArticle = "los";
+
+        public static string GetEntityGenderArticle(bool isFemale)
+        {
+            return (isFemale ? EntityFemaleArticle : EntityMaleArticle);
+        }
+
+        public static string GetEntityGenderArticle(bool isFemale, bool isPlural)
+        {
+            if (isPlural)
+            {
+                return GetEntityGenderPluralArticle(isFemale);
+            }
+            else
+            {
+                return GetEntityGenderArticle(isFemale);
+            }
+        }
+
+        public static string GetEntityGenderPluralArticle(bool isFemale)
+        {
+            return (isFemale ? EntityFemalePluralArticle : EntityMalePluralArticle);
+        }
+
+        #endregion
+
+        #region Error messages
+
         private const string EntityLoadErrorMessage = "Error al cargar {0} {1} por Id.";
         private const string EntityLoadPropertiesErrorMessage = "Error al cargar las propiedades de {0} {1} por Id.";
-        private const string EntityFemaleArticle = "la";
-        private const string EntityMaleArticle = "el";
+        private const string EntityRelatedLoadErrorMessage = "Error al cargar {2} {3} de {0} {1}.";
 
-        public static string GetEntityGenderArticle(bool EntityDisplayNameIsFemale)
+        public static string GetEntityLoadErrorMessage(string entityDisplayName, bool isFemale)
         {
-            return (EntityDisplayNameIsFemale ? EntityFemaleArticle : EntityMaleArticle);
+            return String.Format(EntityLoadErrorMessage, GetEntityGenderArticle(isFemale), entityDisplayName);
         }
 
-        public static string GetEntityLoadErrorMessage(string EntityDisplayName, bool EntityDisplayNameIsFemale)
+        public static string GetEntityLoadPropertiesErrorMessage(string entityDisplayName, bool isFemale)
         {
-            return String.Format(EntityLoadErrorMessage, GetEntityGenderArticle(EntityDisplayNameIsFemale), EntityDisplayName);
+            return String.Format(EntityLoadPropertiesErrorMessage, GetEntityGenderArticle(isFemale), entityDisplayName);
         }
 
-        public static string GetEntityLoadPropertiesErrorMessage(string EntityDisplayName, bool EntityDisplayNameIsFemale)
+        public static string GetEntityRelatedLoadErrorMessage(string entityDisplayName, bool isFemale, string entityRelatedDisplayName, bool relatedIsFemale, bool relatedIsPlural)
         {
-            return String.Format(EntityLoadPropertiesErrorMessage, GetEntityGenderArticle(EntityDisplayNameIsFemale), EntityDisplayName);
+            return String.Format(EntityRelatedLoadErrorMessage, GetEntityGenderArticle(isFemale), entityDisplayName, GetEntityGenderArticle(relatedIsFemale, relatedIsPlural), entityRelatedDisplayName);
         }
+
+        #endregion
+
     }
 }
