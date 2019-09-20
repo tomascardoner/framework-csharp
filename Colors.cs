f276deb7832078c96f20c32df8099414b0e101b3
Tomás A. Cardoner <tomas@cardoner.com.ar>
2019-09-19 22:52:26 -0300

    Create Colors.cs

---
+++ Colors.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+namespace CardonerSistemas
+{
+    class Colors
+    {
+        static public Color SetColor(Color? newColor, Color defaultColor)
+        {
+            if (newColor.HasValue)
+            {
+                return newColor.Value;
+            }
+            else
+            {
+                return defaultColor;
+            }
+        }
+    }
+}
